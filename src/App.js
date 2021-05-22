509f55d93ea025667e4d590aa0515338f05b8f77
brettlungal <lungalb@myumanitoba.ca>
2021-05-21 19:55:24 -0500

    add resume page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,16 @@ function App() {
   return (
     <div className="demo-big-content">
         <Layout>
-            <Header className="header-color" title="Brett Lungal" scroll>
+            <Header className="header-color" title={<Link style={{textDecoration:'none', color:'white'}} to="/">Brett Lungal</Link>} scroll>
                 <Navigation>
-                    <Link to="/">Home</Link>
                     <Link to="/resume">Resume</Link>
                     <Link to="/aboutme">About Me</Link>
                     <Link to="/projects">Projects</Link>
                     <Link to="/contact">Contact</Link>
                 </Navigation>
             </Header>
-            <Drawer title="Brett Lungal">
+            <Drawer title={<Link style={{textDecoration:'none', color:'black'}} to="/">Brett Lungal</Link>}>
                 <Navigation>
-                    <Link to="/">Home</Link>
                     <Link to="/resume">Resume</Link>
                     <Link to="/aboutme">About Me</Link>
                     <Link to="/projects">Projects</Link>
