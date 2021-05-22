945fcb1c2478fea943138347007a619fbd97453a
brettlungal <lungalb@myumanitoba.ca>
2021-05-22 13:43:58 -0500

    finish portfolio website

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,27 +1,63 @@
 import React, {Component} from 'react';
 import {Grid,Cell,Textfield} from 'react-mdl';
 import LetterIcon from '../img/lettericon.jpg'
+import emailjs from 'emailjs-com';
+require('dotenv').config()
+
+const { REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, REACT_APP_USER_ID } = process.env;
 
 class Contact extends Component{
+
+    sendEmail(e) {
+        e.preventDefault();
+        console.log(process.env);
+        emailjs.sendForm(REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, e.target, REACT_APP_USER_ID)
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      }
+
+
+
     render(){
         return(
             <div style={{width:'80%',margin:'auto'}}>
+
                 <Grid>
                     <Cell col={6}>
+                        <h2 style={{marginTop:'25%',marginLeft:'30%',marginBottom:'0%'}}>Contact Me!</h2>
                         <img 
                             src={LetterIcon}
                             alt="letter"
-                            style={{width:'400px',marginTop:'30%',marginLeft:'20%'}}
+                            style={{width:'400px',marginTop:'5%',marginLeft:'20%'}}
                         />
                     </Cell>
                     <Cell col={4}>
                         <div style={{marginTop:'30%'}}>
 
-                        <Textfield
-                            onChange={() => {}}
-                            label="First Name"
-                            style={{width: '400px'}}
-                        />
+                            <form onSubmit={this.sendEmail.bind(this)}>
+                                <div className="form-input">
+                                    <input type="text" className="form-control" placeholder="Name" name="name"/>
+                                </div>
+
+                                <div className="form-input">
+                                    <input type="text" className="form-control" placeholder="Company" name="company"/>
+                                </div>
+
+                                <div className="form-input">
+                                    <input type="text" className="form-control" placeholder="Email" name="email"/>
+                                </div>
+
+                                <div className="form-message">
+                                    <textarea type="text" className="form-control" cols="30" rows="8" placeholder="Message" name="message"/>
+                                </div>
+                                <div className="submit-btn">
+                                    <input type="submit" className="form-btn" value="Send Message" />
+                                </div>
+                            </form>
 
 
                         </div>
