d3f80c64f6eaf4d2e62bcf36559614e7aa9d1786
brettlungal <lungalb@myumanitoba.ca>
2021-12-24 16:20:10 -0600

    add login screen for secret portion of website

--- src/components/main.js
+++ src/components/main.js
@@ -4,6 +4,7 @@ import LandingPage from './landingPage';
 import AboutMe from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
+import SecretLogin from './secretLogin';
 
 const Main = () => (
     <Switch>
@@ -11,6 +12,7 @@ const Main = () => (
         <Route exact path="/aboutme" component={AboutMe}/>
         <Route exact path="/contact" component={Contact}/>
         <Route exact path="/projects" component={Projects}/>
+        <Route excact path="/secretLogin" component={SecretLogin}/>
     </Switch>
 );
 
